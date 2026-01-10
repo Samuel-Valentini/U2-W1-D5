e83ab9dfd9be5c227c7d059cb3d3ce7b33ac4154
Samuel-Valentini <samuelvitalia@gmail.com>
2026-01-10 22:03:45 +0100

    button animatione done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 console.log("script.js online");
 
 const nav = document.querySelector("nav");
+const navButton = document.querySelector("nav button");
 console.log(nav);
 
 window.addEventListener("scroll", () => {
   nav.classList.toggle("whitening", window.scrollY > 350);
+  navButton.classList.toggle("green-painter", window.scrollY > 350);
 });
