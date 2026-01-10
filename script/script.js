39915c843579f9c4d7d79c6197d98f5e987a2090
Samuel-Valentini <samuelvitalia@gmail.com>
2026-01-10 21:42:06 +0100

    scroll animation added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
 console.log("script.js online");
+
+const nav = document.querySelector("nav");
+console.log(nav);
+
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("whitening", window.scrollY > 350);
+});
